1f540c4d8ba09745798fcca6c6fc904cdeaa41a1
Psammy <ampadupsamuel1@gmail.com>
2025-03-05 00:24:21 +0000

    Update

--- App.jsx
+++ App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Header from './Header';
+import Content from './Content';
 function App() {
   return (
     <>
-      <App />
-      <Main />
+      <Header />
+      <Content />
     </>
   );
 }
---
+++ Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Content() {
+  return (
+    <div>
+      <h2>Main content</h2>
+      <p>This is the main content</p>
+    </div>
+  );
+}
+
+export default Content;
