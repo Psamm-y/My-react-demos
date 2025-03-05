a33a3d72a89c33f7dbcc954769cfd1dac4208840
Psammy <ampadupsamuel1@gmail.com>
2025-03-05 12:56:28 +0000

    Update

--- Productinfo.jsx
+++ Productinfo.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const ProductInfo = () => {
-  return <div>Productinfo</div>;
+  const product = {
+    name: 'Computer',
+    price: 1200,
+    availability: 'In-stock',
+  };
+  return (
+    <div>
+      <h4>Product Detials</h4>
+      <li>Product name: {product.name}</li>
+      <li>Price: ${product.price} </li>
+      <li>Availability: {product.availability}</li>
+    </div>
+  );
 };
 
-export default Productinfo;
+export default ProductInfo;
