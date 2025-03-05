a6e29a3b66ca3cf2457d2b2eaa2f0a32a0f88315
Psammy <ampadupsamuel1@gmail.com>
2025-03-05 18:39:05 +0000

    Update

--- App.jsx
+++ App.jsx
@@ -1,15 +1,8 @@
-import React from 'react';
-import Header from './Header';
-import Content from './Content';
-import Footer from './Footer';
-import ProductInfo from './Productinfo.jsx';
+import List from './List';
 function App() {
   return (
     <>
-      <Header />
-      <Content />
-      <Footer />
-      <ProductInfo />
+      <List />
     </>
   );
 }
---
+++ List.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const List = () => {
+  const numbers = [1, 2, 3, 4, 5];
+  const userInfo = [
+    {
+      name: 'Samuel',
+      level: 200,
+      school: 'UG',
+    },
+    {
+      name: 'Kobby',
+      level: 300,
+      school: 'UPSA',
+    },
+    {
+      name: 'David',
+      level: 100,
+      school: 'UG',
+    },
+  ];
+  return (
+    <main>
+      {numbers.map((number) => (
+        <ul key={number}>
+          <li>Number: {number}</li>
+        </ul>
+      ))}
+
+      {userInfo.map(({ name, school, level }) => (
+        <li key={level}>
+          Username: {name} is in {level},{school}
+        </li>
+      ))}
+    </main>
+  );
+};
+
+export default List;
