7aedf9c1ffd2e15a45371fcc476f6e15e76b2c47
Psammy <ampadupsamuel1@gmail.com>
2025-03-05 00:19:44 +0000

    Update

--- App.jsx
+++ App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 function App() {
-  return;
-  <>App;</>;
+  return (
+    <>
+      <App />
+      <Main />
+    </>
+  );
 }
 
 export default App;
---
+++ Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <>
+      <h1>Welcome to my website</h1>
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="">Contact</a>
+    </>
+  );
+}
+
+export default Header;
