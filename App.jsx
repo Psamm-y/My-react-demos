34bb46375cb6c85682434c4592429f89389c7098
Psammy <ampadupsamuel1@gmail.com>
2025-03-05 12:46:32 +0000

    Update

--- App.jsx
+++ App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
+import ProductInfo from './Productinfo.jsx';
 function App() {
   return (
     <>
       <Header />
       <Content />
       <Footer />
+      <ProductInfo />
     </>
   );
 }
