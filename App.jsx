b638de4ea2066bf5eac6703dbea40c5ec233e5e1
Psammy <ampadupsamuel1@gmail.com>
2025-03-06 02:00:21 +0000

    Update

--- App.jsx
+++ App.jsx
@@ -1,9 +1,11 @@
 import Weather from './Weather';
+import UserStatus from './UserStatus';
 function App() {
   const temperature = 35;
   return (
     <>
       <Weather temp={temperature} />
+      <UserStatus loggedin={true} isAdmin={true} />
     </>
   );
 }
