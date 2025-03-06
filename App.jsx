d128faee2a30579269c4fba0bee097d80709f085
Psammy <ampadupsamuel1@gmail.com>
2025-03-06 23:49:05 +0000

    Update

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import Weather from './Weather';
 import UserStatus from './UserStatus';
+import { TfiAlarmClock } from 'react-icons/tfi';
 function App() {
   const temperature = 35;
   return (
     <>
       <UserStatus loggedIn={true} isAdmin={true} />
+      <TfiAlarmClock />
     </>
   );
 }
