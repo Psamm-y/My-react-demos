df3082035bb56607792fef7f36efce1c741c70fd
Psammy <ampadupsamuel1@gmail.com>
2025-03-07 19:04:14 +0000

    Update

--- StateHooks/UseState2.jsx
+++ StateHooks/UseState2.jsx
@@ -1,29 +1,37 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+
+// const UseState2 = () => {
+//   const [friends, setFriends] = useState(['Psammy', 'Alladin']);
+
+//   const addFriend = () => {
+//     setFriends([...friends, 'Ampadu']);
+//   };
+
+//   const removefriend = () => {
+//     setFriends(friends.filter((f) => f !== 'Samuel Ampadu'));
+//   };
+
+//   const updateFriend = () =>
+//     setFriends(friends.map((f) => (f === 'Ampadu' ? 'Samuel Ampadu' : f)));
+//   return (
+//     <section>
+//       {friends.map((f) => (
+//         <li key={Math.random() * 10}>{f}</li>
+//       ))}
+
+//       <button onClick={() => addFriend()}>Add friend</button>
+//       <button onClick={removefriend}>Remove friend</button>
+//       <button onClick={updateFriend}>Update friend</button>
+//     </section>
+//   );
+// };
+
+// export default UseState2;
+
+import React from 'react';
 
 const UseState2 = () => {
-  const [friends, setFriends] = useState(['Psammy', 'Alladin']);
-
-  const addFriend = () => {
-    setFriends([...friends, 'Ampadu']);
-  };
-
-  const removefriend = () => {
-    setFriends(friends.filter((f) => f !== 'Ampadu'));
-  };
-
-  const updateFriend = () =>
-    setFriends(friends.map((f) => (f === 'Ampadu' ? 'Samuel Ampadu' : f)));
-  return (
-    <section>
-      {friends.map((f) => (
-        <li key={Math.random() * 10}>{f}</li>
-      ))}
-
-      <button onClick={() => addFriend()}>Add friend</button>
-      <button onClick={removefriend}>Remove friend</button>
-      <button onClick={updateFriend}>Update friend</button>
-    </section>
-  );
+  return <section>UseState2</section>;
 };
 
 export default UseState2;
