f2a9cee1db6d22d59b39127f1a7e09ae076dcb2d
Psammy <ampadupsamuel1@gmail.com>
2025-03-07 11:36:46 +0000

    Update

--- App.jsx
+++ App.jsx
@@ -1,6 +1,17 @@
 import Weather from './Weather';
 import UserStatus from './UserStatus';
 import { TfiAlarmClock } from 'react-icons/tfi';
+
+const Move = () => {
+  return (
+    <p onCopy={handleCopy}>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum tempore
+      totam ullam repellat rem autem aliquam, quo corporis! Blanditiis at
+      quisquam unde dolor vel et! Et dolores optio necessitatibus delectus.
+    </p>
+  );
+};
+
 function App(props) {
   const temperature = 35;
 
