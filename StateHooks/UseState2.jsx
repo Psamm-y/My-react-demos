1a3f97aba6734b64c1c4479d3de465e343d1c150
Psammy <ampadupsamuel1@gmail.com>
2025-03-07 19:15:19 +0000

    Update

--- StateHooks/UseState2.jsx
+++ StateHooks/UseState2.jsx
@@ -28,22 +28,30 @@
 
 // export default UseState2;
 
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+
+// const UseState2 = () => {
+//   const [movie, setMovie] = useState({
+//     title: 'The Forge',
+//     ratings: 9.9,
+//   });
+
+//   const handeClick = () => setMovie({ ...movie, ratings: 10 });
+//   return (
+//     <section>
+//       <h3>Title: {movie.title}</h3>
+//       <p>Ratings: {movie.ratings}</p>
+//       <button onClick={handeClick}>Change rating</button>
+//     </section>
+//   );
+// };
+
+// export default UseState2;
+
+import React from 'react';
 
 const UseState2 = () => {
-  const [movie, setMovie] = useState({
-    title: 'The Forge',
-    ratings: 9.9,
-  });
-
-  const handeClick = () => setMovie({ ...movie, ratings: 10 });
-  return (
-    <section>
-      <h3>Title: {movie.title}</h3>
-      <p>Ratings: {movie.ratings}</p>
-      <button onClick={handeClick}>Change rating</button>
-    </section>
-  );
+  return <section>UseState2</section>;
 };
 
 export default UseState2;
