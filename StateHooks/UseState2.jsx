443f4dca66d0eddb5affa1b8c869b5269879d361
Psammy <ampadupsamuel1@gmail.com>
2025-03-07 18:43:14 +0000

    Update

--- StateHooks/UseState2.jsx
+++ StateHooks/UseState2.jsx
@@ -3,14 +3,21 @@ import React, { useState } from 'react';
 const UseState2 = () => {
   const [friends, setFriends] = useState(['Psammy', 'Alladin']);
 
+  const addFriend = () => {
+    setFriends([...friends, 'Ampadu']);
+  };
+
+  const removefriend = () => {
+    setFriends(friends.filter((f) => f !== 'Ampadu'));
+  };
   return (
     <section>
       {friends.map((f) => (
-        <li>{f}</li>
+        <li key={Math.random() * 10}>{f}</li>
       ))}
 
-      <button onClick={addFriend}>Add friend</button>
-      <button>Remove friend</button>
+      <button onClick={() => addFriend()}>Add friend</button>
+      <button onClick={removefriend}>Remove friend</button>
       <button>Update friend</button>
     </section>
   );
