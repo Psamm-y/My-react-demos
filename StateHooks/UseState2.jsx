67c60ca49e4d685d40413f0dbbc5d1ebde75f2a1
Psammy <ampadupsamuel1@gmail.com>
2025-03-07 18:34:08 +0000

    Update

--- StateHooks/UseState2.jsx
+++ StateHooks/UseState2.jsx
@@ -2,7 +2,18 @@ import React, { useState } from 'react';
 
 const UseState2 = () => {
   const [friends, setFriends] = useState(['Psammy', 'Alladin']);
-  return <div>UseState2</div>;
+
+  return (
+    <section>
+      {friends.map((f) => (
+        <li>{f}</li>
+      ))}
+
+      <button onClick={addFriend}>Add friend</button>
+      <button>Remove friend</button>
+      <button>Update friend</button>
+    </section>
+  );
 };
 
 export default UseState2;
