05e302a7fd3788e0002083f6fa706f2895d0461b
Psammy <ampadupsamuel1@gmail.com>
2025-03-07 11:52:27 +0000

    Add file

---
+++ StateHooks/USeState.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+const USeState = () => {
+  const styles = {
+    padding: '1rem',
+    margin: '2px',
+  };
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  return (
+    <div>
+      <h1 style={styles}> {count}</h1>
+      <button style={styles} onClick={increment}>
+        +
+      </button>
+      <button style={styles} onClick={decrement}>
+        -
+      </button>
+    </div>
+  );
+};
+
+export default USeState;
