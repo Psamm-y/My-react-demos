9d332e3d569248970a407c62e3014c3c1ba483f1
Psammy <ampadupsamuel1@gmail.com>
2025-03-08 23:47:03 +0000

    Update

--- StateHooks/Todo.jsx
+++ StateHooks/Todo.jsx
@@ -3,18 +3,24 @@ import React, { useState } from 'react';
 const Todo = () => {
   const [tasks, setTasks] = useState(['Do laundry']);
   const [inputValue,setInputValue]= useState("")
-  const handleAdd = (event) => setTasks(event.target.value);
+  const handleAdd = (event) => {
+    setTasks(event.target.value)
+    setInputValue("");
+  };
   return (
     <section>
       <h1>Todo list</h1>
       <form onSubmit={ }>
-              <input type="text" value={tasks} />
+              <input type="text" value={tasks} placeholder='Add todo'/>
 
       </form>
       <button onClick={handleAdd}>Add todo</button>
-      {tasks.map((t) => (
-        <li>{t}</li>
+      <ul>
+          {tasks.map((t,index) => (
+        <li key={index}>{t}</li>
       ))}
+      </ul>
+    
     </section>
   );
 };
