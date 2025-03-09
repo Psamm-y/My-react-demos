2ee0b1be6d957dc17c635ec575a7a9f708ecd2a3
Psammy <ampadupsamuel1@gmail.com>
2025-03-09 00:50:01 +0000

    Update

--- App2.jsx
+++ App2.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Profile from './StateHooks/Profile';
 const App2 = () => {
+  const [user, setUser] = useState({ name: '', age: 5 });
   return (
     <>
       <Profile />
