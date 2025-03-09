9386c32452263294e621447a348054ac4ad38025
Psammy <ampadupsamuel1@gmail.com>
2025-03-09 14:09:04 +0000

    Update

--- Portal/CopyInput.jsx
+++ Portal/CopyInput.jsx
@@ -3,7 +3,17 @@ import React, { useState } from 'react';
 const CopyInput = () => {
   const [inputValue, setInputValue] = useState('');
   const [copied, setCopied] = useState('');
-  return <div>CopyInput</div>;
+
+  const handleCopy = () => {};
+  return (
+    <section>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+    </section>
+  );
 };
 
 export default CopyInput;
