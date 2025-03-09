2e936358249ec1ff84c37aa2bde179c7c51c7f79
Psammy <ampadupsamuel1@gmail.com>
2025-03-09 13:59:57 +0000

    Update

--- App2.jsx
+++ App2.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import ShoppingList from './StateHooks/ShoppingList';
 
 const App2 = () => {
   return (
--- StateHooks/ShoppingList.jsx
+++ StateHooks/ShoppingList.jsx
@@ -5,12 +5,32 @@ const ShoppingList = () => {
   const handleChange = (e) => {
     const { name, value } = e.target;
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (items.name.trim()) {
+      setItems((item) => {
+        items.map(({ name, quantity }) => {
+          name: name;
+          quantity: value;
+        });
+      });
+    }
+  };
   return (
     <div>
       <h2>Shopping List</h2>
       <form onSubmit={handleSubmit}>
-        <input onChange={handleChange} type="text" placeholder="Enter item" />
-        <input onChange={handleChange} type="number" />
+        <label htmlFor="item-name">Name: </label>
+        <input
+          id="item-name"
+          onChange={handleChange}
+          type="text"
+          placeholder="Enter item"
+        />{' '}
+        <br />
+        <label htmlFor="item-quantity">Quantity: </label>
+        <input id="item-quantity" onChange={handleChange} type="number" />
       </form>
     </div>
   );
