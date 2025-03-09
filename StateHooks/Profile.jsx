2e58dc304846474ffcbd0e86ab45f5c25294d61f
Psammy <ampadupsamuel1@gmail.com>
2025-03-09 00:56:43 +0000

    Update

--- StateHooks/Profile.jsx
+++ StateHooks/Profile.jsx
@@ -2,16 +2,33 @@ import React, { useState } from 'react';
 
 const Profile = () => {
   const [user, setUser] = useState({ name: '', age: 5 });
-  const [nameInput, setnameInput] = useState('');
+  const [nameInput, setNameInput] = useState('');
   const [ageInput, setAgeInput] = useState('');
+
+  const handleNameChange = (e) => {
+    setNameInput(e.target.value);
+  };
+  const handleAgeChange = (e) => {
+    setAgeInput(e.target.input);
+  };
   return (
     <div>
       <h2>User Profile</h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor="name"></label>
-        <input id="name" type="text" value={nameInput} />
+        <input
+          onChange={handleNameChange}
+          id="name"
+          type="text"
+          value={nameInput}
+        />
         <label htmlFor="age"></label>
-        <input id="age" type="text" value={ageInput} />
+        <input
+          onChange={handleAgeChange}
+          id="age"
+          type="text"
+          value={ageInput}
+        />
       </form>
     </div>
   );
