64392e3a9446888f8c8025a4f7f624fdb38577b1
Psammy <ampadupsamuel1@gmail.com>
2025-03-09 00:53:41 +0000

    Update

--- StateHooks/Profile.jsx
+++ StateHooks/Profile.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Profile = () => {
   const [user, setUser] = useState({ name: '', age: 5 });
-
+  const [nameInput, setnameInput] = useState('');
+  const [ageInput, setAgeInput] = useState('');
   return (
     <div>
       <h2>User Profile</h2>
-      <form onSubmit={handleSubmit}></form>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name"></label>
+        <input id="name" type="text" />
+        <label htmlFor="age"></label>
+        <input id="age" type="text" value={input} />
+      </form>
     </div>
   );
 };
