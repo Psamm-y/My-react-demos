00716b36d35d3f894dc985e369d34cb964c85bf2
Psammy <ampadupsamuel1@gmail.com>
2025-03-13 23:48:32 +0000

    Update

--- UseEffects/App.jsx
+++ UseEffects/App.jsx
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
   const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch();
+      const data= await 
+    }
+  })
   return <div></div>;
 };
 
