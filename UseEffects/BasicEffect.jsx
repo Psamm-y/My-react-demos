236e1b244341298a2c1fd0cf0c6a527af39a6ff8
Psammy <ampadupsamuel1@gmail.com>
2025-03-14 00:48:34 +0000

    Update

--- UseEffects/BasicEffect.jsx
+++ UseEffects/BasicEffect.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const BasicEffect = () => {
-  return <div>BasicEffect</div>;
+  useEffect(() => {
+    console.log('This message prints only when the component is mounted');
+  }, []); //the empty array depency causes the component to render once
+  //but the message logs twice because our App2 is enclosed in Strictmode in the main.jsx
+  return <div></div>;
 };
 
 export default BasicEffect;
