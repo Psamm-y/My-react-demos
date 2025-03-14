31e6c0e8b72e05a6b74c2cf4f3e21b553622546d
Psammy <ampadupsamuel1@gmail.com>
2025-03-14 01:06:22 +0000

    Update

--- UseEffects/FetchDataEffect.jsx
+++ UseEffects/FetchDataEffect.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const FetchDataEffect = () => {
+  const [data, setData] = useState([]);
+  async function getData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const data = await response.json();
+
+    if (data && data.length) setData(data);
+  }
+  useEffect(() => {});
   return <div>FetchDataEffect</div>;
 };
 
