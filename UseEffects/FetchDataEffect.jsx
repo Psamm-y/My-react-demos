d0909d82559c0cc5cee6f34e8c880c2324d8e91a
Psammy <ampadupsamuel1@gmail.com>
2025-03-14 01:13:10 +0000

    Update

--- UseEffects/FetchDataEffect.jsx
+++ UseEffects/FetchDataEffect.jsx
@@ -2,14 +2,25 @@ import React, { useEffect, useState } from 'react';
 
 const FetchDataEffect = () => {
   const [data, setData] = useState([]);
-  async function getData() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-    const data = await response.json();
 
-    if (data && data.length) setData(data);
-  }
-  useEffect(() => {});
-  return <div>FetchDataEffect</div>;
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/posts'
+      );
+      const data = await response.json();
+
+      if (data && data.length) setData(data);
+    }
+    getData();
+  }, []);
+  return (
+    <div>
+      {data.map((post) => (
+        <li key={post.id}>{post.title && post.id == 1}</li>
+      ))}
+    </div>
+  );
 };
 
 export default FetchDataEffect;
