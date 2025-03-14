d441a327b9754c77211e64e8a63cabb6580bfbb5
Psammy <ampadupsamuel1@gmail.com>
2025-03-14 00:52:39 +0000

    Add CounterEffect.jsx file

--- App2.jsx
+++ App2.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import CounterEffect from './UseEffects/COunterEffect';
 const App2 = () => {
+  const [count, setCount] = useState(0);
   return (
     <>
       <CounterEffect />
---
+++ UseEffects/CounterEffect.jsx
@@ -0,0 +1,10 @@
+//UseEffect with Dependencies
+import React from 'react';
+
+const CounterEffect = () => {
+  const [count, setCount] = useState(0);
+
+  return <div>CounterEffect</div>;
+};
+
+export default CounterEffect;
