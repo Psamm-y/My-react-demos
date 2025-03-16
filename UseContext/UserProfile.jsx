3b691a56725879ae90659d1bf510a8aec6c3e2e4
Psammy <ampadupsamuel1@gmail.com>
2025-03-16 22:10:54 +0000

    Update

---
+++ UseContext/UserProfile.jsx
@@ -0,0 +1,8 @@
+import { UserProvider, UserContext } from './UseContext';
+import React from 'react';
+
+const UserProfile = () => {
+  return <div>UserProfile</div>;
+};
+
+export default UserProfile;
