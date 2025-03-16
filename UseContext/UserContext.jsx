71f6c13b749d755bc4534e94ef48613c1469fcb2
Psammy <ampadupsamuel1@gmail.com>
2025-03-16 23:06:55 +0000

    Update

--- UseContext/UserContext.jsx
+++ UseContext/UserContext.jsx
@@ -1,12 +1,10 @@
-import { createContext,useState } from 'react';
+import { createContext, useState } from 'react';
 
 const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
-  const [user, setUser] - useState({ name: "John Doe" })
-  
-  return <UserContext.Provider>
-    {children}
-  </UserContext.Provider>
-}
-export {UserContext,UserProvider}
+  const [user, setUser] = useState({ name: 'John Doe' });
+
+  return <UserContext.Provider>{children}</UserContext.Provider>;
+};
+export { UserContext, UserProvider };
--- UseContext/UserProfile.jsx
+++ UseContext/UserProfile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { useContext } from 'react';
 const UserProfile = () => {
   return <div>UserProfile</div>;
 };
