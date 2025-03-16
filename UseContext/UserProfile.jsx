365d0e2e34f2a1888f6c45c0a66f044dbb0ccaf0
Psammy <ampadupsamuel1@gmail.com>
2025-03-16 22:25:10 +0000

    Update

--- UseContext/UserProfile.jsx
+++ UseContext/UserProfile.jsx
@@ -2,6 +2,7 @@ import { UserProvider, UserContext } from './UserContext';
 import React, { useContext } from 'react';
 
 const UserProfile = () => {
+  const { user } = useContext(UserContext);
   useContext(UserContext);
   return <div>UserProfile</div>;
 };
