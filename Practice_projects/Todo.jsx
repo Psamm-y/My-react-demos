3c8fc6320bf3f7b93fa5b7ea1e2fd487c347d9b7
Psammy <ampadupsamuel1@gmail.com>
2025-03-18 20:15:58 +0000

    Refactor

--- Practice_projects/Todo.jsx
+++ Practice_projects/Todo.jsx
@@ -1,24 +1,34 @@
 import React, { useState } from 'react';
-
+import './Todo.css';
+import { FcTodoList } from 'react-icons/fc';
 const Todo = () => {
-  const [tasks, setTasks] = useState([{ text: '', completed: false }]);
+  const [tasks, setTasks] = useState([]);
   const [inputValue, setInputValue] = useState('');
 
   const handleInput = (e) => {
     setInputValue(e.target.value);
   };
 
-  const handleAddTodo = () => {
-    setTasks(({ todo }) => [{ ...todo, text: inputValue }]);
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+
+    inputValue.trim() ? setTasks([...tasks, { text: inputValue }]) : tasks.text;
+
     setInputValue('');
   };
   return (
-    <>
-      <h2>My Todo List App</h2>
+    <div className="todo-block tod">
+      <h2>
+        {' '}
+        <FcTodoList style={{ color: 'yellow' }} />
+        My Todo List App
+      </h2>
       <input type="text" onChange={handleInput} value={inputValue} />
       <button onClick={handleAddTodo}>Add todo</button>
-      {tasks.text ? tasks.map((todo) => <li>{todo.text}</li>) : ''}
-    </>
+      {tasks.map((todo, index) => (
+        <li key={index}>{todo.text}</li>
+      ))}
+    </div>
   );
 };
 
