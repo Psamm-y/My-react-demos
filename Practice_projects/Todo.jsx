786b09f8bc9f1657b5e9e3e4886f59890058a3d1
Psammy <ampadupsamuel1@gmail.com>
2025-03-18 19:08:27 +0000

    Refactor

--- Practice_projects/Todo.jsx
+++ Practice_projects/Todo.jsx
@@ -1,8 +1,25 @@
 import React, { useState } from 'react';
 
 const Todo = () => {
-  const [task, setTasks] = useState([{ task: '', completed: false }]);
-  return <div>Todo</div>;
+  const [tasks, setTasks] = useState([{ text: '', completed: false }]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    setTasks(({ todo }) => [{ ...todo, text: inputValue }]);
+    setInputValue('');
+  };
+  return (
+    <>
+      <h2>My Todo List App</h2>
+      <input type="text" onChange={handleInput} value={inputValue} />
+      <button onClick={handleAddTodo}>Add todo</button>
+      {tasks.text ? tasks.map((todo) => <li>{todo.text}</li>) : ''}
+    </>
+  );
 };
 
 export default Todo;
