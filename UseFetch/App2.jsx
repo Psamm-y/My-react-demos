cbb3cd4f8c0a63e305b48fa340e9d4c84f4c7ba1
Psammy <ampadupsamuel1@gmail.com>
2025-03-19 18:43:09 +0000

    Refactor

--- UseFetch/App2.jsx
+++ UseFetch/App2.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 //after using custom hooks (i.e. useFetch)
+import useFetch from './useFetch';
 const App2 = () => {
-  return <div>App2</div>;
+  const [data] = useFetch('https://jsonplaceholder.typicode.com/todos');
+  //destructuring the data array returned by useFetch.jsx
+  return (
+    <div>
+      {data && //to make sure data was fetched before any other thing
+        data.map(
+          (
+            item //let each item in the data array be called 'item'
+          ) => (
+            <p key={item.id}>
+              {item.id}. &nbsp;{item.title} <br /> <br />
+              {/*item is an object with title and id as properties*/}
+            </p>
+          )
+        )}
+    </div>
+  );
 };
 
 export default App2;
---
+++ UseFetch/useFetch.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos') //fecthing the data
+      .then((response) => response.json()) //change to json (JavaScript Object Notation) format
+      .then((data) => setData(data)); //saving the objects acquired in the data array
+  }, []);
+  return [data];
+};
