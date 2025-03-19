3d9e66c5f2ebfffd2cab0fa0363dc04cfbfc9594
Psammy <ampadupsamuel1@gmail.com>
2025-03-19 21:47:27 +0000

    Refactor

--- App2.jsx
+++ App2.jsx
@@ -1,14 +1,14 @@
 // import React, { useState, createContext } from 'react';
 // import { UserContext, UserProvider } from './UseContext/UserContext';
 // import UserProfile from './UseContext/UserProfile';
-import Appp from './UseFetch/Appp';
+import UniqueID from './UseId/UniqueID';
 const App2 = () => {
   return (
     <>
       {/* <UserProvider>
         <UserProfile />
       </UserProvider> */}
-      <Appp />
+      <UniqueID />
     </>
   );
 };
---
+++ UseId/UniqueID.jsx
@@ -0,0 +1,13 @@
+import React, { useId } from 'react';
+
+const UniqueID = () => {
+  const id = useId();
+  return (
+    <div>
+      <label htmlFor={`${id} - email`}>Email</label>
+      <input type="email" id={`${id} - email`} />
+    </div>
+  );
+};
+
+export default UniqueID;
